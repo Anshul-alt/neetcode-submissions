c95b1272e158d565c9299818a70bf368a615de82
Anshul Deodia <36071742+Anshul-alt@users.noreply.github.com>
2026-06-16 09:58:22 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,26 @@
+class Solution { 
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for(let char of s) {
+        if(map[char]) {
+            const top = stack.pop();
+            if(top !==map[char]) {
+                return false;
+            }
+        }
+        else {
+            stack.push(char); // ['[', '(']
+        }
+    }
+    return stack.length === 0;
+    }
+}
